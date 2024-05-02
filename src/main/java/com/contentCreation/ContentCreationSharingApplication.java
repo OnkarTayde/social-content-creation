06a82c60622331dab458a1b3cc0c7dc5b4fe9b23
OnkarTayde <Onkartayade62@gmail.com>
2024-05-03 00:11:40 +0530

    first commit

--- src/main/java/com/contentCreation/ContentCreationSharingApplication.java
+++ src/main/java/com/contentCreation/ContentCreationSharingApplication.java
@@ -9,6 +9,8 @@ public class ContentCreationSharingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ContentCreationSharingApplication.class, args);
 		System.out.println("Hello world");
+
+		System.out.println("1st commit");
 	}
 
 }
